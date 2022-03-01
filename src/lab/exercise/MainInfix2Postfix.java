ee4a7b48adaf0b73a6baf82769588698aed04e5c
pattarapon044 <6431503044@lamduan.mfu.ac.th>
2022-03-01 22:49:46 +0700

    format code

--- src/lab/exercise/MainInfix2Postfix.java
+++ src/lab/exercise/MainInfix2Postfix.java
@@ -16,47 +16,46 @@ public class MainInfix2Postfix {
 		System.out.print("\nPostfix: ");
 		// For each character in the array
 		for (char c : exp) {
-			
+
 			// When char c is not operator
 			if (operator.indexOf(c) == -1) {
-				
+
 				// Check char c is left parenthesis '(' ?
 				if (c == '(') {
 					stack.push(c);
 				}
-				
+
 				// Check char is right parenthesis ')' ?
 				else if (c == ')') {
-					
-					// Pop item out until reach ')' or until stack is  empty
+
+					// Pop item out until reach ')' or until stack is empty
 					for (char e = stack.pop(); e != '('; e = stack.pop()) {
-						
-						if (stack.isEmpty()) {	// Stack empty
+
+						if (stack.isEmpty()) { // Stack empty
 							System.out.println("expression is not matched");
 							break;
 						}
-						
+
 						System.out.print(e);
 					}
 				}
-				
+
 				// When char c is Alphabet
 				else {
 					System.out.print(c);
 				}
 			}
-			
+
 			// When char c is operator
 			else {
-				
+
 				// Check, is stack is empty or not ?
 				if (stack.isEmpty()) {
 					stack.push(c);
-				}
-				else {
+				} else {
 					// Check top of stack
 					char check = stack.peek();
-					
+
 					// If top is operator
 					if (operator.indexOf(check) != -1) {
 						// Pop the exist operator in stack and push new operator to stack.
@@ -64,32 +63,32 @@ public class MainInfix2Postfix {
 						System.out.print(p);
 						stack.push(c);
 					}
-					
+
 					// If top is left parenthesis '('
 					else if (check == '(') {
 						stack.push(c);
 					}
 				}
 			}
-			
+
 		} // end for each character
 
 		// now all operators and operands are checked
 		// is there any operator left in stack?
 		// if stack is NOT empty
 		if (!stack.isEmpty()) {
-			
+
 			// Top is left parenthesis '('
 			if (stack.peek() == '(') {
 				System.out.print("expression is not matched");
 			}
-			
+
 			// Top is operator
 			else {
 				char p = stack.pop();
 				System.out.print(p);
 			}
-			
+
 		} // end if stack is NOT empty
 
 	}// end main method
